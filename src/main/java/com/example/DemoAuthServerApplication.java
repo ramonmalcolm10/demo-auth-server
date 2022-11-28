b61263dc088ee42e6f8480d3d3e6bffe36614563
Ramon Malcolm <ramonmalcolm10@gmail.com>
2022-11-28 06:23:52 -0500

    first commit

---
+++ src/main/java/com/example/DemoAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAuthServerApplication.class, args);
+	}
+
+}
